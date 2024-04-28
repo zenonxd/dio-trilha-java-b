af36f6f13870048dbda2e53803c27ec346f05690
zenonxd <olavomoreira99@gmail.com>
2024-04-28 16:12:40 -0300

    enviando novos projetos

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,13 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+
+        for (String a : alunos) {
+            System.out.println(a);
+        }
+    }
+}
---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,5 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+
+    }
+}
