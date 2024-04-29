6a68eb6ba03fd4d60e5b52f4395f0b17b1d3b591
zenonxd <olavomoreira99@gmail.com>
2024-04-28 21:48:13 -0300

    updating files

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        try {
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser numéricos.");
+        }
+
+
+        scanner.close();
+    }
+}
---
+++ controle-fluxo/src/FormatarCepExemplo.java
@@ -0,0 +1,17 @@
+public class FormatarCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("1235678");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("Cep não corresponde com as regras de negocio.");;
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+        //simulando um cep formatado
+        return "23.765-064";
+    }
+}
